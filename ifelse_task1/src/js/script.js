786ca795646dd7c791a184b86c6c185622fbb375
vikkidanylevska <dva13072016@gmail.com>
2019-03-27 18:53:13 +0200

    script.js is connected

---
+++ ifelse_task1/src/js/script.js
@@ -0,0 +1,16 @@
+var userName = prompt('Введите Ваш логин','');
+
+		if (userName == 'Админ') {
+			var userPassword = prompt('Введите Ваш пароль','');
+			if (userPassword == 'Чёрный Властелин') {
+				alert ('Добро пожаловать!');
+			} else if (userPassword == null){
+				alert ('Вход отменён');
+			} else {
+				alert ('Пароль неверен')
+			}
+		} else if (userName == null) {
+			alert ('Вход отменён');			
+		} else {
+			alert ('Я Вас не знаю!');
+		}
