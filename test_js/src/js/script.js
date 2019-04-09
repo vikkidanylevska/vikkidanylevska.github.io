2b43d05dae740af8a2234a5bc2bcefcbeed94e41
vikkidanylevska <dva13072016@gmail.com>
2019-04-09 09:23:41 +0300

    new change

---
+++ test_js/src/js/script.js
@@ -0,0 +1,168 @@
+;(function IIFE(){
+
+// const random = Math.round(Math.random() * 1000000);
+
+// console.log(random);
+
+// var max = prompt('Ваше число: ');
+
+// alert( Math.random() * max);
+
+// const container = document.createElement('div');
+
+// const table = document.createElement('div');
+// const left = document.createElement('div');
+// left.innerText = 0;
+// const right = document.createElement('div');
+// right.innerText = 0;
+// const btn = document.createElement('button');
+// btn.innerText = 'Start';
+// table.appendChild(left);
+// table.appendChild(right);
+// document.body.appendChild(container);
+// container.appendChild(table);
+// container.appendChild(btn);
+// table.classList.add('game_table');
+// left.classList.add('left');
+// left.classList.add('cube');
+// right.classList.add('right');
+// right.classList.add('cube');
+// btn.classList.add('btn');
+// container.classList.add('container');
+
+// function getPowNumber(x, n) {
+// 	var i=1;
+// 	while (i>n) {
+// 		x*+
+// 	}
+
+// 	return (a > b) ? b : a;
+// }
+
+// var result = getMinNumber(2, 6);
+// alert (result);
+
+// var x = prompt("x?", '');
+// var n = prompt("n?", '');
+
+// var b = +prompt("x?", '');
+
+// if((b % 2) !== 0 || (b % (b-n) !== 0)) {
+
+// 	alert (b);
+
+//  } else {
+
+//    alert ('no');
+
+
+// function number(a,b) {
+
+// 	if (a < b) {
+
+// 	  if ((a % 2)!== 0) {	  	
+// 		alert(a);		
+// 		}
+// 	a++ ;
+// 	number (a,b);
+// 	}
+// }
+
+// number(2, 10);
+// var num = [];
+// function number(a,b) {
+
+// 	if (a < b) {
+
+// 	  function prime(a)   {
+// 	  	j = 2;
+// 	  	c = true;
+// 	  	if (j < a){
+// 	  		if ((a % j) != 0){
+// 				c = false;	 		  		
+// 	  		} 
+// 	  		j++	;
+
+// 	  		if (c = false) {
+// 	  			alert(a)
+// 	  		}
+
+	  		
+// 	  	} else num.push(a);	
+// 	}	 
+
+
+// prime(a)
+ 
+
+// 	a++ ;
+// 	number (a,b);
+
+// 	} else alert(num);
+// }
+
+// number(5, 15);
+
+var playList = [
+  'I Did It My Way',
+  'Respect',
+  'Imagine',
+  'Born to Run',
+  'Louie Louie',
+  'Maybellene'
+];
+
+
+function getPrimesFromRange (start, end){
+	if (!Number.isInteger(+start) && !!Number.isInteger(+end)) 
+		return 'Type a number,please!'
+}
+
+start = +start;
+end = +end;
+
+const primeNumbesArray = [];
+
+for (i=start; i<=+end;i++){
+	if (isPrimeNumber(i)){
+		primeNumbesArray.push(i);
+	}
+}
+
+function isPrimeNumber(n) {
+
+if (n < 2) {
+	return false
+}
+
+for (let i=2; i<n;i++)
+if(n%i===0)
+	return false
+}
+
+return true
+
+return primeNumbesArray;
+
+// var x = prompt("x?", '');
+// var n = prompt("n?", '');
+function pow(n,a=2) {
+	return n**a;
+}
+
+function map(arr, fn=null) {  //arr.map(function())
+
+	const resArr = [];
+	for (let i = 0; i < arr.length; i++)
+		resArr.push (fn(arr[i]))
+}
+
+return resArr
+
+
+map (arr, pow())
+
+}) ();
+
+
+
