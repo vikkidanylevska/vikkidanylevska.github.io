39a04f51bafa1518f594552316b0ce47fef4628c
vikkidanylevska <dva13072016@gmail.com>
2019-06-10 22:35:51 +0300

    finish

--- digital_products/app/js/script.js
+++ digital_products/app/js/script.js
@@ -4,25 +4,15 @@ $(document).ready(function() {
 
 	const topline = document.querySelector('.header');
 
-	const $toTop = $('.scroll-up')
-		$(window).on('scroll', function () {
-			if ($(window).scrollTop() >= 800) {
-				$toTop.fadeIn();
-			} else {
-				$toTop.fadeOut();
-			}
-		});
-
-		$toTop.on('click', function () {
-			$('html, body').animate({ scrollTop: 0 }, 100)
-		});
-
-	document.addEventListener('scroll', () => {
+  document.addEventListener('scroll', () => {
 		if(window.pageYOffset < topline.clientHeight) {
 			topline.classList.remove('fixed');
 		} else {
 			topline.classList.add('fixed');
 		}
+
+  });
+
 
 // Owl Carousel
   $(".slider-works").owlCarousel({
@@ -68,12 +58,35 @@ $(document).ready(function() {
 
 });
 
-});
-
 let map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
+let markerImage = '../img/Pin.svg';
+let marker;
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.0649202, lng: 33.4101731},
+    zoom: 16,
+    disableDefaultUI: true
+  }),
+
+  marker = new google.maps.Marker({
+      position: {lat: 49.0666141, lng: 33.4136125},
+      map: map,
+      animation: google.maps.Animation.BOUNCE,
+      icon: markerImage
+  });
+}
+
+const $toTop = $('.scroll-up')
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() >= 800) {
+        $toTop.fadeIn();
+      } else {
+        $toTop.fadeOut();
       }
+    });
+
+    $toTop.on('click', function () {
+      $('html, body').animate({ scrollTop: 0 }, 800)
+    });
